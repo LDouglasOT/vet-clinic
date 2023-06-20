c49e00c58d9bf89b9401b84db11994983eadd5b2
Adams-Ghub <bamfoadamsf@gmail.com>
2023-06-20 13:30:11 +0000

    Update queries.sql file

--- schema.sql
+++ schema.sql
@@ -67,5 +67,7 @@ CREATE TABLE visits (
   FOREIGN KEY (animal_id) REFERENCES animals(id)
 );
 CREATE INDEX idx_email ON owners (email);
-
+CREATE INDEX idx_animal ON visits(animal_id);
 CREATE INDEX idx_vet_id ON visits (vet_id);
+
+
